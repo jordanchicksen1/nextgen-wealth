041b7218977a982e4bf1b2e8290fde504bd42e65
jordanchicksen1 <2333640@students.wits.ac.za>
2026-06-06 22:32:15 +0200

    eish trying again

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { FinancialProvider } from "./context/FinancialContext";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <FinancialProvider>
-        <App />
-      </FinancialProvider>
-    </StrictMode>
-  </BrowserRouter>
+  <HashRouter>
+  <StrictMode>
+    <FinancialProvider>
+      <App />
+    </FinancialProvider>
+  </StrictMode>
+</HashRouter>
 );
